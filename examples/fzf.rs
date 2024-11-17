c9ccd85811810b97d49caf7653aecded2996c8cc
Alex Rutar <alex@rutar.org>
2024-11-17 14:00:15 +0200

    Improve documentation and add new `fzf` example

---
+++ examples/fzf.rs
@@ -0,0 +1,32 @@
+//! # Simple `fzf` clone
+//!
+//! Read lines from `stdin` in a streaming fashion and populate the picker, immitating the basic
+//! functionality of [fzf](https://github.com/junegunn/fzf).
+use std::{
+    io::{self, BufRead},
+    process::exit,
+    thread::spawn,
+};
+
+use nucleo_picker::{render::StrRenderer, Picker};
+
+fn main() -> io::Result<()> {
+    let mut picker = Picker::new(StrRenderer);
+
+    let injector = picker.injector();
+    spawn(move || {
+        for line in io::stdin().lock().lines() {
+            match line {
+                Ok(s) => injector.push(s),
+                // silently drop io errors!
+                Err(_) => {}
+            }
+        }
+    });
+
+    match picker.pick()? {
+        Some(it) => println!("{it}"),
+        None => exit(1),
+    }
+    Ok(())
+}
