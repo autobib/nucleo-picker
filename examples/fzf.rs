fa2d687f0c6a4d4bbe5a9ed723f876d45e158c22
Alex Rutar <alex@rutar.org>
2025-01-03 16:11:00 +0100

    Do not lock stdin() in fzf example

--- examples/fzf.rs
+++ examples/fzf.rs
@@ -2,11 +2,7 @@
 //!
 //! Read lines from `stdin` in a streaming fashion and populate the picker, imitating the basic
 //! functionality of [fzf](https://github.com/junegunn/fzf).
-use std::{
-    io::{self, BufRead},
-    process::exit,
-    thread::spawn,
-};
+use std::{io, process::exit, thread::spawn};
 
 use nucleo_picker::{render::StrRenderer, Picker};
 
@@ -15,7 +11,7 @@ fn main() -> io::Result<()> {
 
     let injector = picker.injector();
     spawn(move || {
-        for line in io::stdin().lock().lines() {
+        for line in io::stdin().lines() {
             // silently drop IO errors!
             if let Ok(s) = line {
                 injector.push(s);
