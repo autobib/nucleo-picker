6289cc6c439123b690db538ada2519374ccac032
Mark Porter <mark.porter@gbatteries.com>
2025-01-03 18:18:50 -0500

    formatting

--- examples/unrenderable.rs
+++ examples/unrenderable.rs
@@ -1,10 +1,9 @@
-//! # Demonstration of configuration options, some of which are un-renderable, demonstrating the filtering ability 
+//! # Demonstration of configuration options, some of which are un-renderable, demonstrating the filtering ability
 //!
 //! This blocking example demonstrates some of the configuration options available to the picker.
 use std::io::Result;
 
 use nucleo_picker::{nucleo::Config, render::StrRenderer, PickerOptions};
-
 
 fn list_control_characters() -> Vec<char> {
     (0x00..=0x1F).map(|i| char::from_u32(i).unwrap()).collect()
@@ -17,17 +16,13 @@ fn main() -> Result<()> {
         .query("/var")
         .picker(StrRenderer);
 
-
-        
-        
     let control_chars = list_control_characters();
-            
 
     // populate the matcher
     let injector = picker.injector();
     for ctrl in control_chars {
-        let hex_ctrl = format!("{:X}", ctrl as u32);  // Convert the char to a u32 and format as hex
-        let option = format!("hex is {} rendered is {} EOL",hex_ctrl, ctrl);
+        let hex_ctrl = format!("{:X}", ctrl as u32); // Convert the char to a u32 and format as hex
+        let option = format!("hex is {} rendered is {} EOL", hex_ctrl, ctrl);
         injector.push(option);
     }
 
